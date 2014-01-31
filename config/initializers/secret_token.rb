38839ded21158b70bb32d92f7510b95bd308170f
Geoff Cohn <geoffcohn@gmail.com>
2014-01-30 22:48:05 -0600

    Reinitialize after push didn't work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd0c9dd6fb85cf4d41f04c4b8c8f4dfe41aed7482b990d9def40f50d140d987656f59723509d13001faae44b8e94540fc2bcdc049e72ac1fbe0426975055e0f99'
